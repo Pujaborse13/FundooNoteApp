166507d71ab20f69107af4ec766e2884d24cdaa5
Puja Borse <pujaborse13@gmail.com>
2025-04-03 20:45:38 +0530

    Validations applied on

--- CommonLayer/Models/RegisterModel.cs
+++ CommonLayer/Models/RegisterModel.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CommonLayer.Models
 {
     public class RegisterModel
     {
+        [StringLength(20,MinimumLength =3, ErrorMessage ="Provide input minimum 3 letters")]
         public string FirstName { get; set; }
+        
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Provide input minimum 3 letters")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
         public DateTime DOB { get; set; }
         public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage ="Please enter valid Email ID")]        
         public string Email { get; set; }
         public string Password { get; set; }
     }
--- CommonLayer/obj/Debug/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
+++ CommonLayer/obj/Debug/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88684b6abe4fa06cb0877d89c2900215969d7973")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e696162c56a3f46aa3b8ee199cbaca4f4171269")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
